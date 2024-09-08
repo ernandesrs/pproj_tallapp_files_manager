c20f592976a6e7ccc925d982dfa5947dd85ffa10
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-09-08 10:03:52 -0400

    Updating provider

--- src/TallAppFilesManagerProvider.php
+++ src/TallAppFilesManagerProvider.php
@@ -9,20 +9,31 @@ class TallAppFilesManagerProvider extends ServiceProvider
 {
     /**
      * Bootstrap services
+     *
+     * Publish all with: php artisan vendor:publish --provider="Ernandesrs\TallAppFilesManager\TallAppFilesManagerProvider"
+     *
      * @return void
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/tallapp-files-manager.php' => config_path('tallapp-files-manager.php')
-        ], 'config');
-
         $this->loadViewsFrom(__DIR__ . '/resources/views', 'tallapp-files-manager');
         $this->loadMigrationsFrom(__DIR__ . '/migrations');
         $this->loadTranslationsFrom(__DIR__ . '/lang', 'tallapp-files-manager');
 
         Livewire::component('files-manager', \Ernandesrs\TallAppFilesManager\Livewire\FilesManager::class);
         Livewire::component('file-item', \Ernandesrs\TallAppFilesManager\Livewire\FileItem::class);
+
+        $this->publishes([
+            __DIR__ . '/tallapp-files-manager.php' => config_path('tallapp-files-manager.php')
+        ], 'config');
+
+        $this->publishes([
+            __DIR__ . '/lang' => $this->app->langPath('vendor/ernandesrs/tallapp-files-manager')
+        ], 'lang');
+
+        $this->publishes([
+            __DIR__ . '/resources/views' => resource_path('/views/vendor/ernandesrs/tallapp-files-manager')
+        ], 'views');
     }
 
     /**
