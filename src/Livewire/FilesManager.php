fbc1f54f98ead2e9a6f8ca4963759fadbb731448
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-09-08 08:51:01 -0400

    Style adjustments

--- src/Livewire/FilesManager.php
+++ src/Livewire/FilesManager.php
@@ -1,16 +1,104 @@
 <?php
 
 namespace Ernandesrs\TallAppFilesManager\Livewire;
+use Illuminate\Support\Collection;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 
 class FilesManager extends Component
 {
+    #[Url(as: 's', except: null)]
+    public string $search = '';
+
+    #[Url(as: 'type', except: null)]
+    public string $type = 'all';
+
+    public array $typeOptions =
+        [
+            [
+                'label' => 'Todos',
+                'value' => 'all',
+            ],
+            [
+                'label' => 'Vídeos',
+                'value' => 'video',
+            ],
+            [
+                'label' => 'Imagens',
+                'value' => 'image',
+            ],
+            [
+                'label' => 'Documentos',
+                'value' => 'document',
+            ]
+        ];
+
     public function mount()
     {
     }
 
+    /**
+     * Render
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function render()
     {
-        return view('tallapp-files-manager::files-manager');
+        $this->type = empty($this->type) ? 'all' : $this->type;
+
+        return view('tallapp-files-manager::files-manager', [
+            'paths' =>
+                [
+                    [
+                        'text' => 'Vídeos'
+                    ],
+                    [
+                        'text' => '10-10-2024'
+                    ]
+                ],
+            'files' => $this->getFiles(),
+            'directories' => $this->getDirectories()
+        ]);
+    }
+
+    /**
+     * Get files
+     * @return \Illuminate\Support\Collection|\Illuminate\Database\Eloquent\Collection
+     */
+    private function getFiles(): Collection
+    {
+        return Collection::range(0, 23)->map(function () {
+            $type = fake()->randomElement(['video', 'image', 'document']);
+            $extension = fake()->randomElement([
+                'video' => ['mp4', 'mkv', 'mov'],
+                'image' => ['jpg', 'png', 'webp'],
+                'document' => ['doc', 'pdf'],
+            ][$type]);
+
+            return [
+                'id' => 'file_' . fake()->unique()->randomNumber(),
+                'name' => fake()->text(15) . $extension,
+                'type' => $type,
+                'icon' => [
+                    'video' => 'movie',
+                    'document' => 'file',
+                    'image' => 'photo'
+                ][$type],
+                'href' => '#'
+            ];
+        });
+    }
+
+    /**
+     * Get directories
+     * @return \Illuminate\Support\Collection|\Illuminate\Database\Eloquent\Collection
+     */
+    private function getDirectories(): Collection
+    {
+        return Collection::range(0, 11)->map(fn() => [
+            'id' => 'directory_' . fake()->unique()->randomNumber(),
+            'name' => fake()->text(15),
+            'icon' => 'folder',
+            'href' => '#'
+        ]);
     }
 }
--- src/resources/views/files-manager.blade.php
+++ src/resources/views/files-manager.blade.php
@@ -3,31 +3,65 @@
     @component('tallapp-files-manager::includes.container', [
         'header' => true,
     ])
-        <div class="flex-1"></div>
-        <div class="ml-auto">
-            <x-button class="lg:hidden" icon="arrow-up" />
-            <x-button class="hidden lg:flex" icon="arrow-up" text="Upload" />
+        <div class="flex-1 flex flex-col md:flex-row">
+            <div class="flex-1 flex items-center mb-4">
+                <div class="">
+                    <div class="text-sm text-zinc-400 mb-2">Você está em:</div>
+                    <nav class="flex items-center gap-x-2 px-2 text-zinc-500">
+                        @php
+                            $paths = [['text' => 'Home'], ...$paths];
+                        @endphp
+                        @foreach ($paths as $key => $path)
+                            <a wire:navigate class="flex gap-x-1 items-center duration-200 hover:text-zinc-700"
+                                href="{{ $path['href'] ?? null }}"
+                                title="Ir para {{ $path['text'] ?? '' }}">
+                                @if ($path['icon'] ?? false)
+                                    <x-icon name="{{ $path['icon'] }}" />
+                                @endif
+                                @if ($path['text'] ?? false)
+                                    <span>{{ $path['text'] }}</span>
+                                @endif
+                                @isset($paths[$key + 1])
+                                    <span>/</span>
+                                @endisset
+                            </a>
+                        @endforeach
+                    </nav>
+                </div>
+            </div>
+            <div class="md:ml-auto flex justify-start items-center gap-x-2">
+                <form class="flex-1 flex items-center gap-x-2">
+
+                    <div class=" md:min-w-[125px]">
+                        <x-input wire:model.live.debounce.500="search" placeholder="Buscar" />
+                    </div>
+                    <div class=" md:min-w-[125px]">
+                        <x-select.native wire:model.live.debounce.500="type" placeholder="Tipos" :options="$typeOptions"
+                            select="label:label|value:value" />
+                    </div>
+                </form>
+
+                <x-button class="lg:hidden" icon="arrow-up" />
+                <x-button class="hidden lg:flex" icon="arrow-up" text="Upload" />
+            </div>
         </div>
     @endcomponent
 
     @component('tallapp-files-manager::includes.container')
         <div class="flex-1">
             <div class="grid grid-cols-12 gap-2 mb-5">
-                @foreach (\Illuminate\Support\Collection::range(0, 10) as $key => $dir)
+                @foreach ($directories as $key => $dir)
                     @component('tallapp-files-manager::includes.item', [
-                        'text' => 'Dirname',
-                        'icon' => 'folder',
+                        'text' => $dir['name'],
+                        'icon' => $dir['icon'],
                     ])
                     @endcomponent
                 @endforeach
             </div>
             <div class="grid grid-cols-12 gap-2">
-                @foreach (\Illuminate\Support\Collection::range(0, 11) as $key => $dir)
-                    @php
-                        $id = $key + 1;
-                    @endphp
-                    <livewire:file-item wire:id="{{ $id }}" id="{{ $id }}" icon="file"
-                        text="#{{ $key + 1 }} Filena...jpg" />
+                @foreach ($files as $key => $file)
+                    <livewire:file-item id="{{ $file['id'] }}" icon="{{ $file['icon'] }}"
+                        text="{{ $file['name'] }}" />
                 @endforeach
             </div>
         </div>
